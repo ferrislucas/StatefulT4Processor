80a54dabd6284478e1d571ff2b960717812ecae4
Ferris Lucas <ferris.lucas@gmail.com>
2011-04-03 15:26:19 -0500

    change siteframe verbiage

--- src/StatefulT4Processor.BatchManager/Models/ModifyInputModel.cs
+++ src/StatefulT4Processor.BatchManager/Models/ModifyInputModel.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace StatefulT4Processor.TextTemplateBatchManager.Models
 {
 	public class ModifyInputModel
 	{
+		[HiddenInput(DisplayValue = false)]
 		public string Id { get; set; }
+
 		public string Name { get; set; }
+
+		[DisplayName("Zip File")]
 		public string ZipFile { get; set; }
 	}
 }
