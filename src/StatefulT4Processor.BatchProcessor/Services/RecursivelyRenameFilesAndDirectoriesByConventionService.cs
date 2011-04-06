34c90ca0c4f94694ab19403d2995d584fa4305a5
Ferris Lucas <ferris.lucas@gmail.com>
2011-04-06 18:22:35 -0500

    create RecursivelyRenameFilesAndDirectoriesByConventionService.cs

---
+++ src/StatefulT4Processor.BatchProcessor/Services/RecursivelyRenameFilesAndDirectoriesByConventionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StatefulT4Processor.TextTemplateZipProcessor.Services
+{
+	public interface IRecursivelyRenameFilesAndDirectoriesByConventionService
+	{
+		void RecursivelyRename(string path);
+	}
+
+	public class RecursivelyRenameFilesAndDirectoriesByConventionService : IRecursivelyRenameFilesAndDirectoriesByConventionService
+	{
+		public void RecursivelyRename(string path)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
