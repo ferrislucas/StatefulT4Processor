7601795d9d8c3f7c67f04add664036c51830a73d
Ferris Lucas <ferris.lucas@gmail.com>
2011-04-06 19:20:17 -0500

    implement RenameFileOrFolderAccordingToConvention.cs

---
+++ src/StatefulT4Processor.BatchProcessor.Tests/Helpers/RenameFileOrFolderAccordingToConventionTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMoq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using StatefulT4Processor.Shared;
+using StatefulT4Processor.TextTemplateZipProcessor.Helpers;
+
+namespace StatefulT4Processor.TextTemplateZipProcessor.Tests.Helpers
+{
+	[TestClass]
+	public class RenameFileOrFolderAccordingToConventionTests
+	{
+		private AutoMoqer mocker;
+		private readonly Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+		[TestInitialize]
+		public void Init()
+		{
+			mocker = new AutoMoqer();
+			
+			dictionary.Add("TOKEN", "VALUE");
+		}
+
+		[TestMethod]
+		public void Calls_Move_method_of_IFileSystem_with_appropriate_arguments_when_a_filename_is_encountered_with_a_token_in_it()
+		{
+			mocker.Resolve<RenameFileOrFolderAccordingToConvention>().Rename("path/test_-_TOKEN_-_.txt", dictionary);
+
+			mocker.GetMock<IFileSystem>().Verify(a => a.Move("path/test" + RenameFileOrFolderAccordingToConvention.TokenDelimiter +"TOKEN" + RenameFileOrFolderAccordingToConvention.TokenDelimiter + ".txt", "path/testVALUE.txt"), Times.Once());
+		}
+
+		[TestMethod]
+		public void Does_not_call_Move_method_of_IFileSystem_when_there_are_no_tokens_in_the_path()
+		{
+			mocker.Resolve<RenameFileOrFolderAccordingToConvention>().Rename("path/test.txt", dictionary);
+
+			mocker.GetMock<IFileSystem>().Verify(a => a.Move(It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+		}
+
+	}
+}
--- src/StatefulT4Processor.BatchProcessor/Helpers/RenameFileOrFolderAccordingToConvention.cs
+++ src/StatefulT4Processor.BatchProcessor/Helpers/RenameFileOrFolderAccordingToConvention.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using StatefulT4Processor.Shared;
 
 namespace StatefulT4Processor.TextTemplateZipProcessor.Helpers
 {
@@ -12,9 +13,24 @@ namespace StatefulT4Processor.TextTemplateZipProcessor.Helpers
 
 	public class RenameFileOrFolderAccordingToConvention : IRenameFileOrFolderAccordingToConvention
 	{
+		public const string TokenDelimiter = "_-_";
+		private readonly IFileSystem fileSystem;
+
+		public RenameFileOrFolderAccordingToConvention(IFileSystem fileSystem)
+		{
+			this.fileSystem = fileSystem;
+		}
+
 		public void Rename(string path, Dictionary<string, string> state)
 		{
-			throw new NotImplementedException();
+			var newPath = path;
+			foreach (var item in state)
+			{
+				newPath = newPath.Replace(TokenDelimiter + item.Key + TokenDelimiter, item.Value);
+			}
+
+			if (path != newPath)
+				fileSystem.Move(path, newPath);
 		}
 	}
 }
