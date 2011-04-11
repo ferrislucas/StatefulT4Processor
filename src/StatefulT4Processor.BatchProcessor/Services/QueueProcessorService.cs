8154adb8ad35bcf675085957157f817e8d54b355
Ferris Lucas <ferris.lucas@gmail.com>
2011-04-10 23:50:47 -0500

    implement QueueProcessorService

--- src/StatefulT4Processor.BatchProcessor/Services/QueueProcessorService.cs
+++ src/StatefulT4Processor.BatchProcessor/Services/QueueProcessorService.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 using StatefulT4Processor.Shared.Models;
 
 namespace StatefulT4Processor.TextTemplateZipProcessor.Services
@@ -15,7 +19,42 @@ namespace StatefulT4Processor.TextTemplateZipProcessor.Services
 	{
 		public string[] ProcessQueue(Queue queue)
 		{
-			throw new NotImplementedException();
+			string xml;
+			using (var memoryStream = new MemoryStream())
+			{
+				var serializer = new XmlSerializer(typeof(string[]));
+				serializer.Serialize(memoryStream, queue);
+				memoryStream.Position = 0;
+				var sr = new StreamReader(memoryStream);
+				xml = sr.ReadToEnd();
+			}
+
+			var path = string.Format("{0}QueueProcessorWorkingFolder" + Path.DirectorySeparatorChar + "{1}" + Path.DirectorySeparatorChar, ConfigurationManager.AppSettings["PathToLocalWorkingFolder"], Guid.NewGuid());
+			if (!Directory.Exists(path))
+				Directory.CreateDirectory(path);
+			
+			var pathToQueueXmlFile = path + "queue.xml";
+			using (var file = new StreamWriter(pathToQueueXmlFile))
+			{
+				file.Write(xml);
+				file.Close();
+			}
+
+			var p = new Process
+			{
+				StartInfo =
+				{
+					FileName = ConfigurationManager.AppSettings["PathToT4HostConsoleApplication"],
+					Arguments = string.Format("{0}", pathToQueueXmlFile),
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+				}
+			};
+			p.Start();
+
+			var consoleOutput = p.StandardOutput.ReadToEnd();
+
+			return consoleOutput.Split('\n');
 		}
 	}
 }
