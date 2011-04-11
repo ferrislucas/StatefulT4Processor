3fde94ef4daead61214e5ff9681b9112406e1096
Ferris Lucas <ferris.lucas@gmail.com>
2011-04-10 23:38:02 -0500

    implement CreateQueueFromPathService

---
+++ src/StatefulT4Processor.BatchProcessor.Tests/Services/CreateQueueFromPathServiceTests_RecursivelyBuildQueueFromPath.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMoq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatefulT4Processor.Shared;
+using StatefulT4Processor.TextTemplateZipProcessor.Services;
+
+namespace StatefulT4Processor.TextTemplateZipProcessor.Tests.Services
+{
+	[TestClass]
+	public class CreateQueueFromPathServiceTests_RecursivelyBuildQueueFromPath
+	{
+		private AutoMoqer mocker;
+
+		[TestInitialize]
+		public void Init()
+		{
+			mocker = new AutoMoqer();
+		}
+
+		[TestMethod]
+		public void Includes_files_in_first_level_folder()
+		{
+			mocker.GetMock<IFileSystem>().Setup(a => a.GetFiles("path"))
+				.Returns(new string[] {"file1", "file2"});
+
+			var result = mocker.Resolve<CreateQueueFromPathService>().RecursivelyBuildQueueFromPath("path");
+
+			Assert.AreEqual(2, result.QueueItems.Count());
+			Assert.AreEqual(1, result.QueueItems.Where(a => a.InputPath == "file1").Count());
+			Assert.AreEqual(1, result.QueueItems.Where(a => a.InputPath == "file2").Count());
+		}
+
+		[TestMethod]
+		public void Includes_files_in_nested_folder()
+		{
+			mocker.GetMock<IFileSystem>().Setup(a => a.GetDirectories("path"))
+				.Returns(new string[]
+				         	{
+				         		"folder1",
+							});
+			mocker.GetMock<IFileSystem>().Setup(a => a.GetFiles("folder1"))
+				.Returns(new string[] { "file1", "file2" });
+
+			var result = mocker.Resolve<CreateQueueFromPathService>().RecursivelyBuildQueueFromPath("path");
+
+			Assert.AreEqual(2, result.QueueItems.Count());
+			Assert.AreEqual(1, result.QueueItems.Where(a => a.InputPath == "file1").Count());
+			Assert.AreEqual(1, result.QueueItems.Where(a => a.InputPath == "file2").Count());
+		}
+	}
+}
--- src/StatefulT4Processor.BatchProcessor/Services/CreateQueueFromPathService.cs
+++ src/StatefulT4Processor.BatchProcessor/Services/CreateQueueFromPathService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using StatefulT4Processor.Shared;
 using StatefulT4Processor.Shared.Models;
 
 namespace StatefulT4Processor.TextTemplateZipProcessor.Services
@@ -10,9 +12,36 @@ namespace StatefulT4Processor.TextTemplateZipProcessor.Services
 
 	public class CreateQueueFromPathService : ICreateQueueFromPathService
 	{
+		private readonly IFileSystem fileSystem;
+
+		public CreateQueueFromPathService(IFileSystem fileSystem)
+		{
+			this.fileSystem = fileSystem;
+		}
+
 		public Queue RecursivelyBuildQueueFromPath(string path)
 		{
-			throw new NotImplementedException();
+			var queue = new Queue();
+			queue.QueueItems = GetQueueItemsFromPath(path);
+			return queue;
+		}
+
+		public IEnumerable<QueueItem> GetQueueItemsFromPath(string path)
+		{
+			var list = new List<QueueItem>();
+			foreach (var file in fileSystem.GetFiles(path))
+			{
+				list.Add(new QueueItem()
+				         	{
+				         		InputPath = file,
+								
+				         	});
+			}
+			foreach (var folder in fileSystem.GetDirectories(path))
+			{
+				list.AddRange(GetQueueItemsFromPath(folder));
+			}
+			return list;
 		}
 	}
 }
