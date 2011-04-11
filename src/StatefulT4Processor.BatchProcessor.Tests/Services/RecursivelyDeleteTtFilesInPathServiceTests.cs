d2ae64a76cd2cb82f03f180733bbb217067afb20
Ferris Lucas <ferris.lucas@gmail.com>
2011-04-11 00:36:27 -0500

    implement RecursivelyDeleteTtFilesInPathService.cs

---
+++ src/StatefulT4Processor.BatchProcessor.Tests/Services/RecursivelyDeleteTtFilesInPathServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMoq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using StatefulT4Processor.Shared;
+using StatefulT4Processor.TextTemplateZipProcessor.Services;
+
+namespace StatefulT4Processor.TextTemplateZipProcessor.Tests.Services
+{
+	[TestClass]
+	public class RecursivelyDeleteTtFilesInPathServiceTests
+	{
+		private AutoMoqer mocker;
+
+		[TestInitialize]
+		public void Init()
+		{
+			mocker = new AutoMoqer();
+		}
+
+		[TestMethod]
+		public void Deletes_text_templates_but_not_other_files()
+		{
+			mocker.GetMock<IFileSystem>().Setup(a => a.GetFiles("path"))
+				.Returns(new string[] {"file1.tt", "file2.txt"});
+
+			mocker.Resolve<RecursivelyDeleteTtFilesInPathService>()
+				.RecursivelyDeleteTtFilesInPath("path");
+
+			mocker.GetMock<IFileSystem>().Verify(a => a.DeleteFile("file1.tt"), Times.Once());
+			mocker.GetMock<IFileSystem>().Verify(a => a.DeleteFile("file2.txt"), Times.Never());
+		}
+
+		[TestMethod]
+		public void Deletes_text_templates_but_not_other_files_recursively()
+		{
+			mocker.GetMock<IFileSystem>().Setup(a => a.GetDirectories("path"))
+				.Returns(new string[] { "folder1" });
+			mocker.GetMock<IFileSystem>().Setup(a => a.GetFiles("folder1"))
+				.Returns(new string[] { "file1.tt"});
+
+			mocker.Resolve<RecursivelyDeleteTtFilesInPathService>()
+				.RecursivelyDeleteTtFilesInPath("path");
+
+			mocker.GetMock<IFileSystem>().Verify(a => a.DeleteFile("file1.tt"), Times.Once());
+		}
+	}
+}
--- src/StatefulT4Processor.BatchProcessor/Services/RecursivelyDeleteTtFilesInPathService.cs
+++ src/StatefulT4Processor.BatchProcessor/Services/RecursivelyDeleteTtFilesInPathService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using StatefulT4Processor.Shared;
 
 namespace StatefulT4Processor.TextTemplateZipProcessor.Services
 {
@@ -12,9 +13,24 @@ namespace StatefulT4Processor.TextTemplateZipProcessor.Services
 
 	public class RecursivelyDeleteTtFilesInPathService : IRecursivelyDeleteTtFilesInPathService
 	{
+		private IFileSystem fileSystem;
+
+		public RecursivelyDeleteTtFilesInPathService(IFileSystem fileSystem)
+		{
+			this.fileSystem = fileSystem;
+		}
+
 		public void RecursivelyDeleteTtFilesInPath(string path)
 		{
-			throw new NotImplementedException();
+			foreach (var file in fileSystem.GetFiles(path))
+			{
+				if (file.EndsWith(".tt"))
+					fileSystem.DeleteFile(file);
+			}
+			foreach (var directory in fileSystem.GetDirectories(path))
+			{
+				RecursivelyDeleteTtFilesInPath(directory);
+			}
 		}
 	}
 }
